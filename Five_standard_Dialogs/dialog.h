8e42be5a8f081625ffcdabb57d18e26361245332
WZ-Dale <w18829898295@163.com>
2020-04-08 15:59:04 +0800

    五种标准对话框： QFileDialog--文件对话框 QcolorDialog--颜色对话框 QFontDialog--字体对话框 QinputDialog--输入对话框 QMessageBox--消息框

---
+++ Five_standard_Dialogs/dialog.h
@@ -0,0 +1,63 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QDir>
+#include <QFileDialog>
+#include <QColorDialog>
+#include <QFontDialog>
+#include <QInputDialog>
+#include <QMessageBox>
+
+namespace Ui {
+class Dialog;
+}
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Dialog(QWidget *parent = 0);
+    ~Dialog();
+
+private slots:
+    void on_btnOpen_clicked();
+
+    void on_btnOpenMulti_clicked();
+
+    void on_btnSelectDir_clicked();
+
+    void on_btnSave_clicked();
+
+    void on_btnColor_clicked();
+
+    void on_btnFont_clicked();
+
+    void on_btnInputString_clicked();
+
+    void on_btnInputInt_clicked();
+
+    void on_btnInputFloat_clicked();
+
+    void on_btnInputItem_clicked();
+
+    void on_btnMsgInformation_clicked();
+
+    void on_btnMsgWarning_clicked();
+
+    void on_btnMsgCritical_clicked();
+
+    void on_btnMsgAbout_clicked();
+
+    void on_btnMsgQuestion_clicked();
+
+    void on_btnMsgAboutQt_clicked();
+
+    void on_btnCleanText_clicked();
+
+private:
+    Ui::Dialog *ui;
+};
+
+#endif // DIALOG_H
